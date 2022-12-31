e61253624bae9c3974bed7473b1cc30504c85c87
Elif Calık <beyaztahta0@gmail.com>
2022-12-31 10:33:23 +0300

    31/12/2022

---
+++ src/test/java/testcases/Practice/IframeOdev.java
@@ -0,0 +1,46 @@
+package testcases.Practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class IframeOdev extends TestBase {
+    //Bir class olusturun: IframeOdev
+
+    @Test
+    public void test01() throws InterruptedException {
+
+        //https://the-internet.herokuapp.com/iframe  sitesine gidiniz
+        driver.get("https://the-internet.herokuapp.com/iframe ");
+
+        //sayfadaki toplam iframe sayısını bulunuz.
+
+        List<WebElement> list=driver.findElements(By.tagName("iframe"));
+        System.out.println("iframe sayisi = " + list.size());
+
+        //Sayfa basliginda 'Editor' yazını içerdiğini test edelim.
+        String actualPageTitle=driver.findElement(By.xpath("//h3")).getText();
+        String expectedPageTitle="Editor";
+        Assert.assertTrue(actualPageTitle.contains(expectedPageTitle));
+
+        //Paragrafdaki yaziyi silelim
+        driver.switchTo().frame(0);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//p")).clear();
+
+        //Sonrasinda paragrafa "iframein icindeyim" yazisini yazdıralım
+        driver.findElement(By.xpath("//p")).sendKeys(" I am in iframe");
+        Thread.sleep(3000);
+
+        //Alt kısımdaki yazının 'Elemental Selenium' yazisini içerdiğini test edelim
+        driver.switchTo().defaultContent();
+        String actualResult=driver.findElement(By.xpath("//*[@style='text-align: center;']")).getText();
+        String expectedResult="Elemental Selenium";
+        Assert.assertTrue(actualResult.contains(expectedResult));
+    }
+
+}
---
+++ src/test/java/testcases/Practice/WindowHandleHomework.java
@@ -0,0 +1,46 @@
+package testcases.Practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.util.Set;
+
+public class WindowHandleHomework extends TestBase {
+    @Test
+    public void test01() {
+        //https://testcenter.techproeducation.com/index.php?page=multiple-windows
+        driver.get("https://testcenter.techproeducation.com/index.php?page=multiple-windows");
+
+        String windowHandle1 = driver.getWindowHandle();
+
+        //Title in ‘Windows’ oldugunu test edin
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Windows";
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+        //Click here a tiklayin
+        driver.findElement(By.linkText("Click Here")).click();
+
+        //Sonra açılan sayfadaki title in ‘New Window’ oldugunu dogrulayin
+        Set<String> allWindows = driver.getWindowHandles();
+        System.out.println(allWindows);
+        for (String eachHandle : allWindows) {
+            if (!eachHandle.equals(windowHandle1)) {
+                driver.switchTo().window(eachHandle);
+                break;
+            }
+        }
+        String windowHandle2 = driver.getWindowHandle();
+        System.out.println(windowHandle2);
+
+        String expectedResult="New Window";
+        String actualResult = driver.getTitle();
+        Assert.assertEquals(expectedResult,actualResult);
+
+
+    }
+
+
+}
